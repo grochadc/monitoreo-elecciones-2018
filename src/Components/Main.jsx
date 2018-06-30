615cd6802966fb43da15941400e3e7e470e6d7bd
Gonzalo Rocha <grochadc@gmail.com>
2018-06-30 13:57:52 -0500

    Fix state not updating with confirmed true

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -1,38 +1,67 @@
 import React, { Component } from "react";
+import update from "immutability-helper";
 import { toId } from "../lib";
-import db from "../db/newDb.json";
+import db from "../db/newNewDb.json";
 
 class Main extends Component {
   constructor(props) {
     super(props);
     let newvoters = db[props.movilizador].voters.map(voter => {
-      return { name: voter, id: toId(voter) };
+      return {
+        name: voter.name,
+        id: toId(voter.name),
+        confirmed: voter.confirmed
+      };
     });
     this.state = {
       voters: newvoters
     };
     this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleClick = this.handleClick.bind(this);
   }
   handleInputChange(event) {
-    event.preventDefault();
+    let { name } = event.target;
+    this.setState(prevState => {
+      return {
+        voters: [
+          ...prevState.voters.map((voter, i) => {
+            if (i != name) {
+              return voter;
+            } else {
+              return {
+                ...voter,
+                confirmed: true
+              };
+            }
+          })
+        ]
+      };
+    });
+  }
+  handleClick() {
+    console.log(this.state.voters.filter(voter => voter.confirmed));
+    alert("Clicked");
   }
   render() {
     return (
       <div className="Main">
         <h1>{db[this.props.movilizador].movilizador}</h1>
         <ul>
-          {this.state.voters.map(voter => (
-            <li key={voter.id}>
-              <input
-                type="checkbox"
-                name={voter.id}
-                value={voter.id}
-                onChange={e => this.setState({ [voter.id]: true })}
-              />{" "}
-              {voter.name}
-            </li>
-          ))}
-          <button>Guardar</button>
+          {this.state.voters.map(
+            (voter, i) => (
+              <li key={voter.id}>
+                <input
+                  type="checkbox"
+                  name={i}
+                  checked={voter.confirmed}
+                  onChange={event => this.handleInputChange(event)}
+                />{" "}
+                {voter.name}
+              </li>
+            ),
+            this
+          )}
+          <button onClick={this.handleClick}>Guardar</button>
         </ul>
       </div>
     );
